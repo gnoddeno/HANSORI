f71784d16af99916d51eff1d49f03c329a584664
gnoddeno <hanran34@naver.com>
2022-02-14 14:21:33 +0900

    initial commit

---
+++ src/components/hdh.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function hdh(){
+  return (
+    <div className="contents"
+    style={{ height: "100vh", paddingTop: "50vh", backgroundColor: "black" }}
+    >
+      한동호
+    </div>
+  );
+};
+export default hdh;
