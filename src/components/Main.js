c6ed15b3dc1ca39d9473f5878adf04814c9b98b2
gnoddeno <hanran34@naver.com>
2022-02-20 12:40:06 +0900

    비율고정(오류수정)

--- src/components/Main.js
+++ src/components/Main.js
@@ -32,7 +32,7 @@ export default class Main extends Component {
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
-      autoplay: false,
+      autoplay: true,
       autoplaySpeed: 5000,
       cssEase: "ease-in",
       nextArrow: <Arrow image={whiteArrow} next />,
